beb0a7843d8b44d71c4007c771728b906716165d
Ore Akinwole <oreakinwole@gmail.com>
2022-08-15 16:10:56 +0100

    feat: time up modal

---
+++ components/TimeUpModal.js
@@ -0,0 +1,42 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import "react-circular-progressbar/dist/styles.css";
+
+import BaseDialog from "./base/BaseDialog";
+
+export default function TimeUpModal({
+  isModalOpen,
+  setIsModalOpen,
+  points,
+  totalAvailablePoints,
+}) {
+  return (
+    <BaseDialog
+      openModal={isModalOpen}
+      handleClose={() => {
+        setIsModalOpen(false);
+      }}
+      title="Time up"
+    >
+      <div className="">
+        <p>
+          Congratulations, you are done with this assessment, you scored
+          <b className=""> {points}points</b> out of{" "}
+          <b>{totalAvailablePoints}</b>
+        </p>
+      </div>
+      <br />
+      <div className="">
+        <ButtonGroup justifyContent="center" className="">
+          <Button
+            variant="outline"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            Close
+          </Button>
+        </ButtonGroup>
+      </div>
+    </BaseDialog>
+  );
+}
