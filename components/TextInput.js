de1b5a172f3a337e293fe47f5c86ac0605609161
Ore Akinwole <oreakinwole@gmail.com>
2022-08-15 16:10:34 +0100

    feat: challenge and user text input

---
+++ components/TextInput.js
@@ -0,0 +1,32 @@
+import { Textarea } from "@chakra-ui/react";
+
+export default function TextInput({
+  heading,
+  handleOnKeyUp,
+  handleUserInput,
+  handleWriteUpInput,
+  startTimeValue,
+}) {
+  return (
+    <>
+      <small>{heading}</small>
+      <Textarea
+        size="lg"
+        h={40}
+        mb={4}
+        placeholder={heading}
+        _placeholder={{ color: "black" }}
+        // Using this for only user input
+        onKeyUp={(e) => {
+          handleOnKeyUp(e.key);
+        }}
+        onInput={(e) => {
+          handleUserInput(e.target.value);
+        }}
+        // Using this only for the challenge input
+        onChange={(e) => handleWriteUpInput(e.target.value)}
+        disabled={!startTimeValue}
+      />
+    </>
+  );
+}
