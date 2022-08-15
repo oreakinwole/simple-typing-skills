848ff842dfa2b63ae82bd0efc241051414a210f7
Ore Akinwole <oreakinwole@gmail.com>
2022-08-15 16:11:32 +0100

    chore: init test suite

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import { render, screen, } from '@testing-library/react'
+// import Home from '@/pages/index'
+
+describe('Home', () => {
+  it('renders component', () => {
+    // render(<Home />)
+
+    // const heading = screen.getByRole('heading', {
+    //   name: /welcome to next\.js!/i,
+    // })
+
+    // Fail test case
+      expect('h1').toBeChecked()
+    })
+})
